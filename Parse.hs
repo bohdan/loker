6450393459afb98cb11057b261c56a3116c255ff
Roman Cheplyaka <roma@ro-che.info>
2010-02-21 13:17:44 +0200

    Add parameter expansion

--- Parse.hs
+++ Parse.hs
@@ -1,18 +1,50 @@
 import Data.List
 import Text.Parsec hiding (tokens)
-import Control.Applicative ((<$>))
+import Data.Maybe
 
 type S = String
+
+-- A parameter can be denoted by a name, a number, or one of the special
+-- characters listed in Special Parameters.
+--
+-- A variable is a parameter denoted by a name.
+--
+-- A positional parameter is a parameter denoted by the decimal value
+-- represented by one or more digits, other than the single digit 0.
+data Parameter = Var String
+               | Positional Int
+               | Special Char
+    deriving Show
+
+data SubstringType = Suffix | Prefix
+    deriving Show
+data PatternType = Largest | Smallest
+    deriving Show
+data CheckType = CheckUnset | CheckUnsetAndNull
+    deriving Show
+
+data ParModifier = UseDefault       Word CheckType
+                 | AssignDefault    Word CheckType
+                 | Assert           Word CheckType
+                 | UseAlternative   Word CheckType
+                 | StringLength
+                 | Remove           Word PatternType SubstringType
+                 | NoModifier
+    deriving Show
+
+data ParSubstExpr = ParSubstExpr Parameter ParModifier
+    deriving Show
 
 data WordPart = Bare String
                | SQuoted String
                | DQuoted [WordPart]
                | Escaped Char
                | CommandSubst [WordPart]
-               | Parameter [WordPart]
+               | ParSubst ParSubstExpr
                | Arith [WordPart]
     deriving Show
 
+type Word = [WordPart]
 data Token = Word [WordPart]
            | Op String
     deriving Show
@@ -27,12 +59,12 @@ singleQuoted = do
     squote = char '\''
     nonQuote = satisfy (/= '\'')
 
-bareWord :: Parsec S u WordPart
-bareWord = do
+bareWord :: String -> Parsec S u WordPart
+bareWord terminators = do
     word <- many1 ordinarySymbol
     return $ Bare word
     where
-    ordinarySymbol = noneOf $ "'\"`$\\\n# " ++ opFirstLetters
+    ordinarySymbol = noneOf terminators
 
 escaped :: Parsec S u WordPart
 escaped = do
@@ -43,7 +75,7 @@ escaped = do
 doubleQuoted :: Parsec S u WordPart
 doubleQuoted = do
     dQuote
-    parts <- many1 $ escaped <|> bareWord <|> substitution
+    parts <- many1 $ escaped <|> bareWord "$`\\\"" <|> substitution
     dQuote
     return $ DQuoted parts
     where
@@ -51,12 +83,11 @@ doubleQuoted = do
     escaped = do
         char '\\'
         fmap Escaped $ oneOf "$`\"\\\n"
-    bareWord = fmap Bare $ (many1 $ noneOf "$`\\\"")
 
-word :: Parsec S u Token
-word = do
-    parts <- many1 $ escaped <|> singleQuoted <|> doubleQuoted <|> substitution <|> bareWord
-    return $ Word parts
+word :: String -> Bool -> Parsec S u Word
+word terminators acceptEmpty = do
+    (if acceptEmpty then many else many1) $
+        escaped <|> singleQuoted <|> doubleQuoted <|> substitution <|> bareWord terminators
 
 --- Operators ---
 
@@ -80,10 +111,11 @@ whiteSpace = do many1 $ comment <|> (char ' ' >> return ());
                 return ()
 
 --- Substitutions ---
+-- Parameter expansion, command substitution or arithmetic expansion
 substitution :: Parsec S u WordPart
 substitution = do
     char '$'
-    parameter
+    fmap ParSubst parameterSubst
 
 -- A word consisting solely of underscores, digits, and alphabetics from the
 -- portable character set. The first character of a name is not a digit. 
@@ -97,29 +129,80 @@ name = do
     letter = satisfy $ \x -> (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z')
     underscore = char '_'
 
-parameter :: Parsec S u WordPart
-parameter = do
-    fmap Parameter $ braced <|> unbraced
+parameterSubst :: Parsec S u ParSubstExpr
+parameterSubst = do
+    braced <|> unbraced <?> "parameter substitution"
     where
     lbrace = char '{'
     rbrace = char '}'
-    braced = do
-        lbrace
-        expr <- many (escaped <|> singleQuoted <|> doubleQuoted <|> bare)
-        rbrace
-        return expr
-        where
-        escaped = do char '\\'; fmap Escaped $ char '}'
-        bare = fmap Bare $ many1 $ satisfy (/= '}')
 
-    unbraced = fmap (:[]) $ simple_positional <|> fmap Bare name
-    simple_positional = fmap (Bare.(:[])) digit
+    braced = between lbrace rbrace $
+        try string_length <|> 
+        try parameter_check <|> 
+        try parameter_substr <|>
+        try (flip ParSubstExpr NoModifier `fmap` parameter)
+
+    parameter = do special <|> positional <|> fmap Var name 
+
+    word_arg = word "}'\"`$\\" True
+
+    parameter_check = do
+        par <- parameter
+        colon <- optionMaybe $ char ':'
+        op <- oneOf "-+=?"
+        w <- word_arg
+        
+        let mod = case op of
+             '-' -> UseDefault
+             '=' -> AssignDefault
+             '+' -> UseAlternative
+             '?' -> Assert
+        
+        let checkType = if isJust colon then CheckUnsetAndNull else CheckUnset
+
+        return $ ParSubstExpr par (mod w checkType)
+
+    parameter_substr = do
+        par <- parameter
+        op <- choice $ map (try.string) ["%%","%","##","#"]
+        w <- word_arg
+
+        let mod = case op of
+             "%"  -> Remove w Smallest Suffix
+             "%%" -> Remove w Largest Suffix
+             "#"  -> Remove w Smallest Prefix
+             "##" -> Remove w Largest Prefix
+        return $ ParSubstExpr par mod
+
+    string_length = do
+        char '#'
+        par <- parameter
+        return $ ParSubstExpr par StringLength
+
+    unbraced = do
+        par <- special -- should be the first to capture 0
+           <|> simple_positional
+           <|> fmap Var name
+        return $ ParSubstExpr par NoModifier
+    
+    variable = fmap Var name <?> "variable"
+
+    simple_positional = do
+        d <- digit
+        return $ Positional $ read [d]
+
+    positional = do
+        n <- many1 digit
+        return $ Positional $ read n
+    
+    special = fmap Special $ oneOf "@*#?-$!0"
 
 --- Tokens ---
 tokens :: Parsec S u [Token]
 tokens = do
     optional whiteSpace
-    sepEndBy (word <|> operator) (optional whiteSpace)
+    sepEndBy (token_word <|> operator) (optional whiteSpace)
+    where token_word = fmap Word (word ("'\"`$\\\n# " ++ opFirstLetters) False)
 
 main = do
     s <- getContents
