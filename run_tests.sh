663793fb2676289dc2da78d1bb2c89815fcc3200
Bohdan Vlasyuk <bohdan@dhcp-172-28-209-61.zrh.corp.google.com>
2010-03-20 18:26:39 +0100

    golden test framework

---
+++ run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+golden=0
+[[ $1 == "--golden" ]] && { golden=1; shift; }
+[[ $# != 0 ]] && { echo "Usage: $0 [--golden]"; exit 1; }
+parse() { runghc Parse "$@" ; }
+for f in tests/*.golden; do
+  test_input="${f/.golden}"
+  diff -u "$f" <(parse < $test_input) && echo PASS || {
+    [[ $golden == 1 ]] && {
+      echo "Rewriting the golden file: $f"
+      parse < $test_input > $f
+    }
+  }
+done
